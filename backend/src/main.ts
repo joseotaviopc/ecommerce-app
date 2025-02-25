e2b706d70c43abc8f2d51e3588e7751e460c8cfd
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-25 11:21:54 -0300

    feat: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste mobile')
+    .setDescription('API para o teste mobile')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log(`Server running on http://localhost:${PORT}`))
+  .catch((e) => console.log('Something went wrong...', e));
