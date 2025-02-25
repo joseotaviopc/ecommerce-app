69cf88278091f87ecb7a604793463f36e5203797
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-25 17:48:22 -0300

    feat: add api status route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOperation({ description: 'Get the status of the API' })
+  @ApiOkResponse({ description: 'API status' })
+  @ApiInternalServerErrorResponse({ description: 'Something went wrong' })
+  getStatus(): string {
+    return this.appService.getStatus();
   }
 }
