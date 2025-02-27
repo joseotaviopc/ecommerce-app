990bf4b1bd1504cc093539969a7f8418cead3819
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-27 11:43:44 -0300

    feat: load fonts and create a button component

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
+import { NotoSans_400Regular as NotoSans_400, NotoSans_500Medium as NotoSans_500, NotoSans_600SemiBold as NotoSans_600, NotoSans_700Bold as NotoSans_700 } from '@expo-google-fonts/noto-sans'
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
@@ -17,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    NotoSans_400, NotoSans_500, NotoSans_600, NotoSans_700,
     ...FontAwesome.font,
   });
 
@@ -41,9 +42,9 @@ export default function RootLayout() {
 function RootLayoutNav() {
 
   return (
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
   );
 }
---
+++ mobile/components/button.tsx
@@ -0,0 +1,71 @@
+import colors from '@/constants/Colors';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Pressable, StyleSheet, Text, TextProps, TouchableOpacity, ViewProps } from 'react-native';
+
+type ButtonProps = {
+    title: string;
+    onPress: () => void;
+    variant?: 'gradient' | 'outline' | 'outlineColor' | 'danger';
+};
+
+export default function Button({ title, onPress, variant = 'gradient' }: ButtonProps) {
+    const buttonVariants: Record<string, ViewProps['style']> = {
+        outline: {
+            backgroundColor: 'transparent',
+            borderWidth: 1,
+            borderColor: colors.darker,
+        },
+        outlineColor: {
+            backgroundColor: 'transparent',
+            borderWidth: 1,
+            borderColor: colors.green,
+        },
+        danger: {
+            backgroundColor: 'transparent',
+            borderWidth: 1,
+            borderColor: colors.red,
+        },
+    }
+    const textVariants: Record<string, TextProps['style']> = {
+        danger: {
+            color: colors.red,
+        },
+    }
+
+    if (variant === 'gradient') {
+        return (
+            <TouchableOpacity onPress={onPress} activeOpacity={0.7} style={{ width: '100%' }}>
+                <LinearGradient
+                    colors={[colors.gradientFrom, colors.gradientTo]}
+                    start={[0, 0]}
+                    end={[1, 1]}
+                    style={[styles.button]}>
+                    <Text style={styles.text}>{title}</Text>
+                </LinearGradient>
+            </TouchableOpacity>
+        )
+    }
+    return (
+        <TouchableOpacity onPress={onPress} style={[styles.button, buttonVariants[variant]]} activeOpacity={0.7}>
+            <Text style={[styles.text, textVariants[variant]]}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        paddingVertical: 8,
+        paddingHorizontal: 32,
+        borderRadius: 36,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+    text: {
+        color: colors.darker,
+        fontSize: 16,
+        fontFamily: 'NotoSans_600',
+        lineHeight: 20,
+    },
+});
