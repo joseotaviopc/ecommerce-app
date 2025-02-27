12d6573f242562e7340e253a492a4f53d79d311b
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-27 10:30:40 -0300

    chore: create expo mobile project

---
+++ mobile/constants/Colors.ts
@@ -0,0 +1,27 @@
+const green = '#28CE9C'
+const lightBlue = '#6AC9FF'
+const darkBlue = '#007CC2'
+const red = '#ED3540'
+const yellow = '#FFCD48'
+const darker = '#132322'
+const dark = '#3A4241'
+const light = '#EDEDED'
+const white = '#FFFFFF'
+const gradientFrom = '#6AC9FF'
+const gradientTo = '#28CE9C'
+
+const colors = {
+  green,
+  lightBlue,
+  darkBlue,
+  red,
+  yellow,
+  darker,
+  dark,
+  light,
+  white,
+  gradientFrom,
+  gradientTo,
+}
+
+export default colors;
