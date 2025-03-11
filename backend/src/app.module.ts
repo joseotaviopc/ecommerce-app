41790b57c47c443adabd02d3b20d5da927b96abc
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-03-11 11:08:42 -0300

    feat: add order and address modules in app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CartModule],
+  imports: [UsersModule, ProductsModule, CartModule, OrderModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
